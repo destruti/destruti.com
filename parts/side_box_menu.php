30b69655741353dfc398c2b4e4dab8408510113e
Eduardo Destruti <limaedu@gmail.com>
2026-08-21 10:39:29 -0500

    bugfix

--- parts/side_box_menu.php
+++ parts/side_box_menu.php
@@ -2,6 +2,7 @@
     <ul class="stats-list">
         <li><a href="https://www.linkedin.com/in/eduardodestruti/" target="_blank">LinkedIn Page</a></li>
         <li><a href="https://www.instagram.com/elimaedu/" target="_blank">Instagram Page</a></li>
-        <li><a href="/certs">Certifications</a></li>
+        <li><a href="/graduations/">Graduations</a></li>
+        <li><a href="/certificates/">Certificates</a></li>
     </ul>
 </div>
