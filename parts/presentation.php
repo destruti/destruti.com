90827825b67350a9a9b97f018ae86c9a59f64540
Eduardo Destruti <limaedu@gmail.com>
2026-08-21 12:40:48 -0500

    bugfix

--- parts/presentation.php
+++ parts/presentation.php
@@ -8,6 +8,6 @@
     <p>Married to Fernanda; father of May, Maria, and Zoey.</p>
     <div class="profile-meta">
         <span>📍 Tyler, TX - USA 🇺🇸</span>
-        <span>Married to Fernanda; father of Manuela, Maria Eduarda, and Zoey.</span>
+        <span>CTO Stuff and Hubspot 24/7!</span>
     </div>
 </div>
--- parts/side_box_presentation.php
+++
@@ -1,4 +0,0 @@
-<div class="sidebar-box">
-    <h3>Currently learning</h3>
-    <p>CTO Stuff and Hubspot 24/7!</p>
-</div>
