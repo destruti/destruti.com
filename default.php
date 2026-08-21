e7fb91e90985565454ec894e31965a436370af55
Eduardo Destruti <limaedu@gmail.com>
2026-08-21 10:08:15 -0500

    bugfix

---
+++ default.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manu Duda Zoey</title>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: white;
+            color: #006400;
+            /* Verde escuro */
+            font-family: 'Press Start 2P', cursive;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 2rem;
+            margin: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Manu Duda Zoey</h1>
+</body>
+
+</html>
---
+++ scripts/gitpush.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# chmod +x gitpush.sh
+# chmod +x /Users/vmbfesalviano/vmb/vmb_shma/scripts/gitpush.sh && /Users/vmbfesalviano/vmb/vmb_shma/scripts/gitpush.sh "Adiciona funcionalidade de scripts"
+# sh scripts/gitpush.sh
+
+# Armazena a mensagem de commit
+# COMMIT_MSG="$1"
+COMMIT_MSG="${1:-bugfix}"
+
+git pull
+
+# Adiciona todos os arquivos modificados
+git add .
+
+# Faz o commit com a mensagem fornecida
+git commit -m "$COMMIT_MSG"
+
+# Faz o push para o repositório de prod
+git push prod main
+
+# Faz o push para o repositório remoto (branch atual)
+git push origin main
+
+# Mensagem de conclusão
+echo "Push realizado com sucesso! ("$COMMIT_MSG")"
